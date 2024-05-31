96280f0214e60e4aecf3bb3888731d2ed9a521be
djalbe <acaramean@mail.ru>
2024-06-01 01:28:32 +0300

    Add new Controllers

--- src/main/java/com/air/airstore/Controllers/TicketControllers.java
+++ src/main/java/com/air/airstore/Controllers/TicketControllers.java
@@ -1,10 +1,65 @@
 package com.air.airstore.Controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.air.airstore.EntityDTO.TicketEntityDTO;
+import com.air.airstore.Service.EntitiesService.ServiceImpl.TicketServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
 public class TicketControllers {
+
+    private final TicketServiceImpl tickerService;
+
+
+    private static final Logger logger = LoggerFactory.getLogger(TicketControllers.class);
+
+
+    @Autowired
+    public TicketControllers(TicketServiceImpl tickerService) {
+        this.tickerService = tickerService;
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<TicketEntityDTO>> getAllTickets() {
+        try{
+            return ResponseEntity.ok(tickerService.getTickets());
+    }catch (Exception e){
+            logger.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+    }
+
+    @GetMapping("/all/{id}")
+    public ResponseEntity<TicketEntityDTO> getTicketById(@PathVariable Long id) {
+
+        try{
+            return ResponseEntity.ok(tickerService.getTicketById(id));
+
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<TicketEntityDTO> addTicket(@RequestBody TicketEntityDTO ticketDTO, @PathVariable Long id) {
+        try{
+            TicketEntityDTO ticketEntityDTO = tickerService.createTicket(ticketDTO,id);
+            return new ResponseEntity<>(ticketEntityDTO, HttpStatus.CREATED);
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+    }
 }
